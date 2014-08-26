4f7251a7368217c77df5194c16cac720eb817413
Michael Conlon <mconlon@ufl.edu>
2014-08-26 11:55:11 -0400

    Upgraded for 1.5 improvement project

--- merge_uris.py
+++ merge_uris.py
@@ -12,12 +12,12 @@
     publication.  You want to merge x and y so that the publication is assigned
     to the full profile at x and the redundant profile at y is removed.  In
     this case the from is "y" and the to is "x".  After the merge, all the
-    things that were said about y (publication, anything else) is assigned to
+    things that were said about y (publication, anything else) are assigned to
     x.  y, and all facts about y, are removed.
 
     Note:  merge_uris is remarkably dangerous.  It will merge ANY two uris.
     merge_uri should be used carefully and as part of a comprehensive activity
-    to matain the data quality of a VIVO triple store.
+    to maintain the data quality of a VIVO triple store.
 
     Version 0.0 MC 2013-11-30
     --  complete draft
@@ -26,26 +26,34 @@
     --  Uses csv file to drive bulk merges.
     Version 0.2 MC 2014-06-21
     --  Use functions from vivotools
-    Version 0.3 MC 2012-06-22
-    --  Code style improvements.
+    Version 0.3 MC 2014-06-22
+    --  Code style improvements
+    Version 0.4 MC 2014-08-26
+    --  Updated for new tools, add command line argument for filename
 """
 
 __author__ = "Michael Conlon"
 __copyright__ = "Copyright 2014, University of Florida"
 __license__ = "BSD 3-Clause license"
-__version__ = "0.3"
+__version__ = "0.4"
 
-from vivotools import rdf_header
-from vivotools import rdf_footer
-from vivotools import read_csv
-from vivotools import merge_uri
+from vivofoundation import rdf_header
+from vivofoundation import rdf_footer
+from vivofoundation import read_csv
+from vivofoundation import merge_uri
 from datetime import datetime
 import codecs
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help="File containing from and to URIs",
+                    default="2014-08-26.txt")
+args = parser.parse_args()
 
 print datetime.now(), "Start"
 ardf = rdf_header()
 srdf = rdf_header()
-rows = read_csv("merge_countries.txt")
+rows = read_csv(args.filename)
 for i in sorted(rows.keys()):
     row = rows[i]
     ardf = ardf + "\n<!-- Merge " + row["from"] + " to " + row["to"] + " -->\n"
